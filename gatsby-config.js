6208c52cbd099f4481dba284af5420914618c892
Leslie Owusu-Appiah <leslie@blank.works>
2020-06-22 11:15:06 +0100

    Bugfix: Firefox partner images

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#000000`,
         theme_color: `#000000`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
